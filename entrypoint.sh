fe60afb56bd5d080e08a9be3d317997e46fcc400
Christopher W. Butler <cbutler.msstate@gmail.com>
2019-02-23 15:53:01 -0500

    Run dev server in debug mode

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,9 @@
 
 python3 manage.py collectstatic --no-input
 python3 manage.py migrate --no-input
-gunicorn lazyguys.wsgi -b 0.0.0.0:${PORT} --preload --max-requests 1200
+
+if [ -z ${DEBUG+x} ]; \
+then gunicorn lazyguys.wsgi -b 0.0.0.0:${PORT} --preload --max-requests 1200; \
+else python3 manage.py runserver 0.0.0.0:${PORT}; fi
 
 exec "$@"
