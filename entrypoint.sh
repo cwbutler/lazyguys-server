87f46c2c00c70dfec0fa66fdf1e047d9b4cae555
Christopher W. Butler <cbutler.msstate@gmail.com>
2019-02-24 21:36:59 -0500

    run python dev server if not in production

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,12 @@
 
 python3 app/manage.py collectstatic --no-input
 python3 app/manage.py migrate --no-input
-gunicorn lazyguys.wsgi --bind 0.0.0.0:${PORT} --preload --max-requests 1200 --chdir ./app -w 2
+
+if [ "DJANGO_DEBUG" == "False" ]
+then
+  gunicorn lazyguys.wsgi --bind 0.0.0.0:${PORT} --preload --max-requests 1200 --chdir ./app -w 2
+else
+  python3 app/manage.py runserver 0.0.0.0:${PORT}
+fi
 
 exec "$@"
