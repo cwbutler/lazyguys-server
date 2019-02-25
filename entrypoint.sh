dd5f95004575a40eb846295cbe24a618f90d49b9
Christopher W. Butler <cbutler.msstate@gmail.com>
2019-02-24 19:15:22 -0500

    another heroku fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 python3 app/manage.py collectstatic --no-input
 python3 app/manage.py migrate --no-input
-gunicorn lazyguys.wsgi --bind 0.0.0.0:8000 --preload --max-requests 1200 --chdir ./app -w 2
+gunicorn lazyguys.wsgi --bind 0.0.0.0:${PORT} --preload --max-requests 1200 --chdir ./app -w 2
 
 exec "$@"
