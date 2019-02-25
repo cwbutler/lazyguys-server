2d43a5a7998a4cda36b838b51d9d0f414d67c184
Christopher W. Butler <cbutler.msstate@gmail.com>
2019-02-25 11:16:32 -0500

    updated nginx and entrypoint config

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-python3 app/manage.py collectstatic --no-input
 python3 app/manage.py migrate --no-input
 
 if [ "DJANGO_DEBUG" == "False" ]
 then
-  gunicorn lazyguys.wsgi --bind 0.0.0.0:${PORT} --preload --max-requests 1200 --chdir ./app -w 2
+  python3 app/manage.py collectstatic --no-input
+  gunicorn lazyguys.wsgi --bind 0.0.0.0:${APP_PORT} --preload --max-requests 1200 --chdir ./app -w 2
 else
-  python3 app/manage.py runserver 0.0.0.0:${PORT}
+  python3 app/manage.py runserver 0.0.0.0:${APP_PORT}
 fi
 
 exec "$@"
